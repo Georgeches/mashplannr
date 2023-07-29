a3c1d8a4623a942116bf8d33af76015f0ac8b983
FeisalKeli <kellyfeisal2000@gmail.com>
2023-07-29 13:44:25 +0300

    Routing Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
-//
-import Login from "./components/login"; // Make sure the path is correct
-
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import LoginForm from "./components/auth/LoginForm";
 import RegisterForm from "./components/auth/RegisterForm";
 import Header from "./components/Header";
-import { Routes, Route } from "react-router-dom";
-
-import { useEffect, useState } from "react";
+import Login from "./components/login";
 
 function App() {
   const [user, setUser] = useState(null);
+
   useEffect(() => {
     // auto-login
     fetch("/me").then((r) => {
@@ -19,17 +18,19 @@ function App() {
     });
   }, []);
 
-  if (!user) return <Login onLogin={setUser} />;
-
   function handleLogout() {
     setUser(null);
   }
+
   return (
     <div className="App">
       <Header onLogout={handleLogout} />
       <Routes>
-        <Route path="/merchant" element={<LoginForm />} />
-        <Route path="/merchant" element={<RegisterForm />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login onLogin={setUser} />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/merchant/login" element={<LoginForm />} />
+        <Route path="/merchant/register" element={<RegisterForm />} />
       </Routes>
     </div>
   );

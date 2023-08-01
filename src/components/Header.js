4da6e494f72ced986863673702a6861965a7c279
Georgeches <georgechesire83@gmail.com>
2023-08-01 17:14:32 +0300

    make icons visible when width is minimal

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header({ handleLogout }) {
         <div className="container-fluid">
               <nav className="navbar container-fluid justify-content-between align-items-center">
             <div className="navbar-header">
-            <button className="btn btn-link d-lg-none text-dark border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNav" aria-controls="offcanvasTop"><i className="fa-solid fa-bars" style={{fontSize: "25px"}}></i></button>
+            
               <a href="/" className="navbar-brand logo">
                 {/* Your logo image */}
               </a>
@@ -76,66 +76,6 @@ function Header({ handleLogout }) {
         </div>
       </header>
 
-      {/**offcanvas*/}
-      <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasNav" aria-labelledby="offcanvasTopLabel">
-                <div className="offcanvas-header">
-                    <h4 id="offcanvasTopLabel"></h4>
-                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div className="offcanvas-body">
-                <div className="main-menu-wrapper">
-              <div className="menu-header">
-                <a href="/" className="menu-logo">
-                  {/* Your logo image */}
-                </a>
-              </div>
-              <ul className="main-nav">
-                <li className="has-submenu megamenu">
-                  <a href="/">
-                    Home
-                  </a>
-                  <ul className="submenu mega-submenu">
-                    {/* ... Mega Submenu Content ... */}
-                  </ul>
-                </li>
-                <li className="has-submenu megamenu">
-                  <Link to="/merchants">
-                    Merchants
-                  </Link>
-                  <ul className="submenu mega-submenu">
-                    {/* ... Mega Submenu Content ... */}
-                  </ul>
-                </li>
-
-                <li className="has-submenu">
-                  <a href="/login">
-                    Admin
-                  </a>
-                  <ul className="submenu">{/* ... Submenu Content ... */}</ul>
-                </li>
-
-                <li className="login-link">
-                  <a href="/login">Login / Signup</a>
-                </li>
-                <li className="register-btn">
-                  <a
-                    href="/login
-                  "
-                    className="btn reg-btn"
-                  >
-                    <i className="feather-user"></i>Register
-                  </a>
-                </li>
-                <li className="register-btn">
-                  <a href="/logout" className="btn btn-primary log-btn">
-                    <i onClick={handleLogout} className="feather-lock"></i>
-                    Logout
-                  </a>
-                </li>
-              </ul>
-            </div>
-                </div>
-            </div>
     </div>
   );
 }
